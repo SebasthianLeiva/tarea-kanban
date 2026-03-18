33859e3603f2630f8a2ecc2b3f17ca2b66424711
Sebasthian Leiva <sebasthianleiva473@gmail.com>
2026-03-18 18:45:43 -0300

    creacion de enum Estado y creacion de atributos y metodos de Tarea

---
+++ src/main/java/dominio/Estado.java
@@ -0,0 +1,9 @@
+package dominio;
+
+public enum Estado {
+	
+	POR_HACER,
+	EN_PROCESO,
+	TERMINADO
+
+}
--- src/main/java/dominio/Tarea.java
+++ src/main/java/dominio/Tarea.java
@@ -1,5 +1,52 @@
 package dominio;
 
 public class Tarea {
+	
+	private String titulo;
+	private String texto;
+	private Estado estado; 
+	
+	public Tarea(String titulo, String texto) {
+		
+		this.titulo = titulo;
+		this.texto= texto;
+		this.estado = Estado.POR_HACER;
+		
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public Estado getEstado() {
+		return estado;
+	}
+
+	public void setEstado(Estado estado) {
+		this.estado = estado;
+	}
+	
+	
+	@Override
+	
+	public String toString() {
+		
+		return titulo; 
+		
+	}
+	
+	
 
 }
