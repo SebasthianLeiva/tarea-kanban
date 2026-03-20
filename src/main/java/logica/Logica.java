973ec45fc2ce7d1855ccc00410206791ee25ae45
Sebasthian Leiva <sebasthianleiva473@gmail.com>
2026-03-20 01:44:50 -0300

    se agregaron los archivos de texto para guardar y cargar estado. se añadieron los metodos guardarEstado y CargarEstado

--- src/main/java/logica/Logica.java
+++ src/main/java/logica/Logica.java
@@ -1,5 +1,10 @@
 package logica;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import dominio.Estado;
@@ -94,6 +99,151 @@ public class Logica {
 	
 	
 	
+	
+	
+	public void guardarEstado() {
+		
+		try {
+			
+			//se escribe el primer archivo
+			
+			FileWriter fw1 = new FileWriter("tareasPorHacer.txt");
+			
+			for(Tarea tarea : tareasPorHacer) {
+						
+				fw1.write(tarea.getTexto() + "\n");
+				
+			}
+			
+			fw1.close();
+			
+			
+			//se escribe el segundo archivo
+			
+			FileWriter fw2 = new FileWriter("tareasEnProceso.txt");
+			
+			for(Tarea tarea : tareasEnProceso) {
+						
+				fw2.write(tarea.getTexto() + "\n");
+				
+			}
+			
+			fw2.close();
+			
+			
+			//se escribe el tercer archivo
+			
+			FileWriter fw3 = new FileWriter("tareasTerminado.txt");
+			
+			for(Tarea tarea : tareasTerminado) {
+						
+				fw3.write(tarea.getTexto() + "\n");
+				
+			}
+			
+			fw3.close();
+			
+			
+			
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		
+		
+	}
+	
+	
+	
+	
+	
+	public void cargarEstado() {
+		
+		tareasPorHacer.clear();  //se limpian los arrayList para evitar sobrecargar al cargar 2 veces seguidas. 
+		tareasEnProceso.clear();
+		tareasTerminado.clear(); 
+		
+	
+		
+		//se lee el primer archivo
+		
+		try {
+			FileReader fr = new FileReader("tareasPorHacer.txt");
+			
+			BufferedReader bf = new BufferedReader(fr); 
+
+				
+				String linea;
+				
+			while((linea= bf.readLine()) != null) {
+					
+				Tarea tarea = new Tarea(linea);
+					
+				tareasPorHacer.add(tarea);
+					
+					
+			}
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		
+		
+		//se lee el segundo archivo
+		
+		try {
+			
+			FileReader fr = new FileReader("tareasEnProceso.txt");
+			
+			BufferedReader bf = new BufferedReader(fr); 
+			
+			String linea;
+			
+			
+			while((linea= bf.readLine()) != null) {
+					
+			Tarea tarea = new Tarea(linea);
+				
+			tareasEnProceso.add(tarea);
+					
+					
+			}
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		//se lee el tercer archivo
+		
+		try {
+			FileReader fr = new FileReader("tareasTerminado.txt");
+			
+			BufferedReader bf = new BufferedReader(fr); 
+			
+			String linea;
+			
+			while((linea= bf.readLine()) != null) {
+					
+					Tarea tarea = new Tarea(linea);
+					
+					tareasTerminado.add(tarea);
+					
+					
+				}
+			
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		
+		
+		
+	}
 	
 	
 	
